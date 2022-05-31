eb3d02291624afc11e27a0bc6d9b3fb031b05e4c
Alex <al.chumakov96@gmail.com>
2022-06-01 01:06:37 +0800

    small issue

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['fast-ocean-38849.herokuapp.com']
+ALLOWED_HOSTS = ['fast-ocean-38849.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
