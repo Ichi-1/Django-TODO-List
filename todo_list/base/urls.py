2ed69dc041f0e348707fc67fdcf279b636a22ef5
Alex <al.chumakov96@gmail.com>
2022-06-06 08:30:00 +0800

    edit: .gitignore and .md

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', SingUpPage.as_view(), name='rootpage'),
     path('sign-up', SingUpPage.as_view(), name='sign-up'),
     path('login', CustomLogin.as_view(), name='login'),
-    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('task', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task-id'),
     path('task/create/', TaskCreate.as_view(), name='task-create'),
